6d136155c4c78360eb44593bb5fab3b385a50560
AZAR2305 <thameemulazarudeen.27csb@licet.ac.in>
2025-07-29 21:01:42 +0530

    About route creation

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import SwapSimulation from "./pages/SwapSimulation";
 import ActivityLog from "./pages/ActivityLog";
 import Governance from "./pages/Governance";
 import NotFound from "./pages/NotFound";
-
+import About from "./pages/About";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -26,6 +26,7 @@ const App = () => (
           <Route path="/swap" element={<SwapSimulation />} />
           <Route path="/activity" element={<ActivityLog />} />
           <Route path="/governance" element={<Governance />} />
+          <Route path="/about" element={<About />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
